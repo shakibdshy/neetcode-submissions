fd129e891040748350749412776dc320b2f7ec9b
Md Habibur Rahman <shakibdshy@gmail.com>
2026-05-05 10:07:48 +0600

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+
+        const count = {};
+
+        for (let i = 0; i < s.length; i++) {
+            count[s[i]] = (count[s[i]] || 0) + 1;
+            count[t[i]] = (count[t[i]] || 0) - 1;
+        }
+
+        for (let key in count) {
+            if (count[key] !== 0) return false;
+        }
+
+        return true;
+    }
+}
